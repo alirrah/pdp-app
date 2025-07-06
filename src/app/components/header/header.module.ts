ed36166d7b274d65b8ad27a08d63437b657295c5
alirrah <alir.rah81@gmail.com>
2025-07-06 19:49:10 +0330

    [REMOVE] test files

--- src/app/components/header/header.module.ts
+++ src/app/components/header/header.module.ts
@@ -2,8 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule, NgOptimizedImage } from '@angular/common';
 import { RouterLink } from '@angular/router';
 
-import { HeaderComponent } from './header.component';
 import { MingcuteMenuLineModule } from '../../../assets/icons/mingcute-menu-line/mingcute-menu-line.module';
+
+import { HeaderComponent } from './header.component';
 
 @NgModule({
   declarations: [HeaderComponent],
--- src/assets/icons/mingcute-telegram-fill/mingcute-telegram-fill.module.ts
+++ src/assets/icons/mingcute-telegram-fill/mingcute-telegram-fill.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
 import { MingcuteTelegramFillComponent } from './mingcute-telegram-fill.component';
 
 @NgModule({
