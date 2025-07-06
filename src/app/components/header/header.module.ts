92228356ec499ad0b40f5fcc095eaa823444b594
alirrah <alir.rah81@gmail.com>
2025-07-06 18:49:12 +0330

    [EDIT] header icon and items

--- src/app/components/header/header.module.ts
+++ src/app/components/header/header.module.ts
@@ -3,10 +3,11 @@ import { CommonModule, NgOptimizedImage } from '@angular/common';
 import { RouterLink } from '@angular/router';
 
 import { HeaderComponent } from './header.component';
+import { MingcuteMenuLineModule } from '../../../assets/icons/mingcute-menu-line/mingcute-menu-line.module';
 
 @NgModule({
   declarations: [HeaderComponent],
   exports: [HeaderComponent],
-  imports: [CommonModule, NgOptimizedImage, RouterLink],
+  imports: [CommonModule, NgOptimizedImage, RouterLink, MingcuteMenuLineModule],
 })
 export class HeaderModule {}
